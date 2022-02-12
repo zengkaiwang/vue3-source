4ca80334b0a52ba9482362da82ce21b8da50cf81
scaler <zengkaiwang@yeah.net>
2022-02-12 16:39:26 +0800

    feat: vue3响应式模块的使用

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+import { reactive, effect, ref, computed } from '@vue/reactivity'
+
+// proxy 代理
+const state = reactive({
+  name: 'scaler',
+  age: 18,
+})
+
+effect(() => {
+  console.log(state.name)
+})
+
+state.name = 'wzk'
