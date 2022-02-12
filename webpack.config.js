170e8a57ffcf8a10fb282796a7a1e18b6baae359
scaler <zengkaiwang@yeah.net>
2022-02-12 14:56:43 +0800

    feat: 使用webpack手动搭建vue3源码的开发环境

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    })
+  ]
+}
